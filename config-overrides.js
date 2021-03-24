c45c387a4bf1f05055314baab6ee80af4071cf94
vmask <jsjlish@163.com>
2021-03-24 15:59:46 +0800

    配置webpack alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,11 @@
+const path = require('path')
+
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
     //配置别名
     config.resolve.alias = {
-        '@': resolve('src')
+        '@': path.resolve(__dirname, 'src')
     };
-    
+
     return config;
   }
